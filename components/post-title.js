c9614cae09d0b994081f055c18af1e7a5dd50fa7
Toky <io@solotoky.de>
2022-02-02 10:55:41 +0100

    revert title and add kicker

--- components/post-title.js
+++ components/post-title.js
@@ -1,7 +1,7 @@
-export default function PostKicker({ children }) {
+export default function PostTitle({ children }) {
   return (
-    <h6 className="text-6sl md:text-7sl lg:text-8sl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
-    </h6>
+    </h1>
   );
 }

a7c3466f1d186692fdae3cbf3f31f9fb07c893c8
Toky <io@solotoky.de>
2022-02-02 10:57:17 +0100

    improve kicker

--- components/post-kicker.js
+++ components/post-kicker.js
@@ -1,4 +1,4 @@
-xport default function PostKicker({ children }) {
+export default function PostKicker({ children }) {
   return (
       <h6 className="text-6sl md:text-7sl lg:text-8sl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
         {children}
